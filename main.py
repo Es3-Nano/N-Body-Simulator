5ba659263333a152e189aed27b58b2755108b0ed
Lawrence <lawrenceashun@gmail.com>
2026-07-09 23:12:11 +0000

    My first circle boys •ᴗ•

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
 from sys import exit
 
-pygame.init
-screen = pygame.display.set_mode((800, 400))
+pygame.init()
+sim_screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Simulator :)")
 clock = pygame.time.Clock()
+
+color = (255, 0, 0)
+test_surface = pygame.Surface((100, 200))
+center = (100, 100)
+radius = 75
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    
+    pygame.draw.circle(sim_screen, color, center, radius)
     
     pygame.display.update()
     clock.tick(60)
