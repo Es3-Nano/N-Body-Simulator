dcd0bbbc9eeaf4782a7cb11df2b92142055018af
Lawrence <lawrenceashun@gmail.com>
2026-07-09 23:59:25 +0000

    The first falling circle. Took a while to figure out but TLDR, it seems Pygame draws things in layers. However, you wouldn't actually see the results till you update the display so if filling the screen with black is the last thing you do all the player is going is see it black since it has covered the circle and the player sees the final outcome. Simple for the experienced but as a newbie I feels are though that I turely coding something I understand the workings of. Good progress so far, I just need to keep it up and go further ദ്ദി_(ツ)

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import pygame
 from sys import exit
 
+y_pos = 50
+
 pygame.init()
-sim_screen = pygame.display.set_mode((800, 400))
+screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Simulator :)")
 clock = pygame.time.Clock()
 
-color = (255, 0, 0)
-test_surface = pygame.Surface((100, 200))
-center = (100, 100)
-radius = 75
-
 while True:
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    
-    pygame.draw.circle(sim_screen, color, center, radius)
+
+    y_pos += 1
+    pygame.draw.circle(screen, (255, 0, 0), (400, y_pos), 20)
     
     pygame.display.update()
     clock.tick(60)
