c93bef7f00949d85bb70a4fc647a53c273950da0
Lawrence <lawrenceashun@gmail.com>
2026-07-10 00:17:19 +0000

    I got a circle going up and down now. Wow it is late (╥﹏╥)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from sys import exit
 
 y_pos = 50
+flip_state = False
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
@@ -9,14 +10,23 @@ pygame.display.set_caption("Simulator :)")
 clock = pygame.time.Clock()
 
 while True:
+    if flip_state == False:
+        y_pos += 1
+    elif flip_state == True:
+        y_pos -=1
+
     screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    y_pos += 1
     pygame.draw.circle(screen, (255, 0, 0), (400, y_pos), 20)
+
+    if 380 <= y_pos:
+        flip_state = True
+    elif 20 >= y_pos:
+        flip_state = False
     
     pygame.display.update()
     clock.tick(60)
