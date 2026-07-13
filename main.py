08ae97b7f51c6285639f0182530ca58536d62574
Lawrence <lawrenceashun@gmail.com>
2026-07-13 18:39:45 +0000

    Also, one last tweak. I forgot to accumulate acceleration, I accidentally override it it. My bad >_<

--- main.py
+++ main.py
@@ -11,16 +11,15 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Simulator :)")
 clock = pygame.time.Clock()
 
-core = bodies.Body(10000, 0, 0, (255, 220, 50), 0, 0)
+black_hole = bodies.Body(30000, 0, 0, (40, 40, 40), 0, 0)
 
-planet_1 = bodies.Body(80, 200, 0, (255, 0, 0), 0, -0.5)
-planet_2 = bodies.Body(80, -200, 0, (0, 255, 0), 0, 0.5)
+star_1 = bodies.Body(50, 150, 0, (255,255,255), 0, -5.5)
+star_2 = bodies.Body(50, 250, 50, (255,100,100), -1, -4.8)
+star_3 = bodies.Body(50, 350, 100, (100,255,255), -1.5, -4)
 
-planet_3 = bodies.Body(60, 0, 350, (0, 0.500, 255), 0.5, 0)
-planet_4 = bodies.Body(60, 0, -350, (255, 0, 255), -0.5, 0)
-
-planet_5 = bodies.Body(40, 450, 200, (255, 255, 255), -0.5, 0.5)
-planet_6 = bodies.Body(40, -450, -200, (255, 120, 0), 0.5, -0.5)
+star_4 = bodies.Body(50, -150, 0, (255,255,0), 0, 5.5)
+star_5 = bodies.Body(50, -250, -50, (255,0,255), 1, 4.8)
+star_6 = bodies.Body(50, -350, -100, (100,255,100), 1.5, 4)
 
 def run_simulator():
     while True:
@@ -30,7 +29,7 @@ def run_simulator():
                 pygame.quit()
                 exit()
 
-        for l in range(1000):
+        for l in range(100):
             c_engine.calculate_force(0.01)
 
         bodies.Body.draw_all_bodies(screen)
