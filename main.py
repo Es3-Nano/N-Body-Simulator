c3076bbcae2ddf3604aa85bd9dfc06656d602fc0
Lawrence <lawrenceashun@gmail.com>
2026-08-14 20:23:50 +0000

    Good news: Numba parallel is hela abusive so I got to 5000 bodies at 30 fps by brute force. Bad news: Merging doesn't work so now I have to rethink how to handle collisions and merging. Whether I process one at a time or handle many at the same time.

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ import bodies
 import physics_engine
 import scipy.stats as stats
 import random as rd
-import numpy as np
 import time
 
 dt = 0.01
 response_trys = 5
-allowCollision = True
+allowCollision = None
+
+def start():
+    global allowCollision
+    print("Welcome to my N-body Simulator :]")
+    body_count = check_if_int("Enter number of bodies you want to simulate: ", 1000)
+    collision_confrim = check_if_valid("Do you want collisions (y for yes, n for no? ", "y", ["n", "y"])
+
+    if collision_confrim == "y":
+        allowCollision = True
+    else:
+        allowCollision = False
+
+    normal_distribution(body_count)
 
 def check_if_int(prompt: str, deflaut: int):
+
     for x in range(response_trys):
         try:
             response = input(prompt)
@@ -40,10 +53,7 @@ def check_if_valid(prompt, deflaut, response_list: list):
     print(f"Deflauting to {deflaut}")
     return deflaut
 
-def normal_distribution():
-    print("Welcome to my N-body Simulator :]")
-    body_count = check_if_int("Enter number of bodies you want to simulate: ", 25)
-    
+def normal_distribution(body_count):    
     x_coordinates = []
     y_coordinates = []
 
@@ -52,16 +62,16 @@ def normal_distribution():
 
     mu_x = 0
     mu_y = 0
-    sp = 300
-    mass_select = 0
+    sp = 30
+    mass_select = None
 
     x_a, x_b = -x_radius / sp, x_radius / sp
     y_a, y_b = -y_radius / sp, y_radius / sp
 
-    x_coordinates = stats.truncnorm.rvs(x_a, x_b, mu_x, sp, body_count).tolist()
-    y_coordinates = stats.truncnorm.rvs(y_a, y_b, mu_y, sp, body_count).tolist()
+    x_coordinates = stats.truncnorm.rvs(x_a, x_b, mu_x, sp, body_count)
+    y_coordinates = stats.truncnorm.rvs(y_a, y_b, mu_y, sp, body_count)
 
-    mass_request = check_if_valid("Do you want randon masses (r) or same mass (s) for all bodies: ", "s", ["s", "r"])
+    mass_request = check_if_valid("Do you want randon masses (r) or same mass (s) for all bodies? ", "s", ["s", "r"])
     
     if mass_request == "s":
         mass_select = check_if_int("Enter the mass you want for all bobies: ", 100)
@@ -72,14 +82,13 @@ def normal_distribution():
         lowest_mass = check_if_int("Enter the lowest mass you want for any body: ", 100)
         highest_mass = check_if_int("Enter the highest mass you want for any body: ", 1000)
 
-        mass_range = [ rd.randint(lowest_mass, highest_mass) for _ in range(body_count) ]
+        mass_range = [rd.randint(lowest_mass, highest_mass) for _ in range(body_count)]
         for m, x, y in zip(mass_range, x_coordinates, y_coordinates):
             bodies.create_body(m, x, y)
 
     bodies.intialize_acc()
 
 def run_simulator():
-    # Initializing Pygame Window
     pygame.init()
     screen = pygame.display.set_mode((bodies.screen_width, bodies.screen_height))
     pygame.display.set_caption("Simulator ツ")
@@ -90,19 +99,15 @@ def run_simulator():
     print("Running")
 
     while running:
+
         frame_start = time.perf_counter()
 
         screen.fill((0, 0, 0))
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        # C_Engine Part
         start = time.perf_counter()
-
-        b_pairs = np.arange(bodies.number_of_bodies)
-
         for _ in range(1):
             collision_check = physics_engine.calculate_force(
                 bodies.mass, 
@@ -112,10 +117,15 @@ def run_simulator():
                 bodies.acc_y, 
                 bodies.b_radii, 
                 bodies.collision_list, 
-                collision_check,
-                b_pairs)
-            if collision_check and allowCollision:
-                bodies.collision(bodies.collision_list[0], bodies.collision_list[1], physics_engine.eps)
+                collision_check)
+
+            if collision_check and allowCollision and len(bodies.collision_list) >= 2:
+                bodies.collision(
+                    bodies.collision_list[0],
+                    bodies.collision_list[1],
+                    physics_engine.eps)
+                bodies.collision_list.clear()
+
             bodies.update_pos(dt)
         physics_time = time.perf_counter() - start
 
@@ -127,7 +137,7 @@ def run_simulator():
 
         # FPS
         clock.tick(30)
-        fps = 1.0 / frame_time if frame_time > 0 else 0
+        fps = 1 / frame_time if frame_time > 0 else 0
 
         print(
             f"FPS: {fps:.2f} | "
@@ -138,5 +148,5 @@ def run_simulator():
 
     pygame.quit()
 
-normal_distribution()
+start()
 run_simulator()
--- physics_engine.py
+++ physics_engine.py
@@ -1,29 +1,38 @@
 import math
-from numba import njit
+from numba import njit, prange
+import bodies
 
-eps = 0.1
+eps = bodies.b_radii
 
-@njit
+@njit(parallel=True, fastmath=True)
 def calculate_force(mass, x_pos, y_pos, acc_x, acc_y, radii, c_list, c_status):
     c_status = False
-    for i, (p1_x, p1_y) in enumerate(zip(x_pos, y_pos)):
+    num_particles = len(x_pos)
+
+    for i in prange(num_particles):
+        p1_x = x_pos[i]
+        p1_y = y_pos[i]
+        
         x_dis = y_dis = a_mag = a_y = a_x = 0
 
-        for j, (p2_m, p2_x, p2_y) in enumerate(zip(mass, x_pos, y_pos)):
+        for j in prange(num_particles):
             if i == j:
                 continue
+
+            p2_m = mass[j]
+            p2_x = x_pos[j]
+            p2_y = y_pos[j]
         
             x_dis = p2_x - p1_x
             y_dis = p2_y - p1_y
             r = math.sqrt((x_dis ** 2) + (y_dis ** 2))   
 
-            if r <= radii:
-                if not c_status:
+            if r <= radii and c_status == False:
                     c_list[0] = i
                     c_list[1] = j
                     c_status = True
                              
-            a_mag = (p2_m) / ((r ** 2) + (eps ** 2))
+            a_mag = (p2_m) / ((r ** 2) + (eps))
             a_x += a_mag * (x_dis / r)
             a_y += a_mag * (y_dis / r)
 
